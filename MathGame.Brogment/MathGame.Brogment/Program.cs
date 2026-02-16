993d4a5a854e8ca83a971617049b206b4995e2d6
brogment <140029632+brogment@users.noreply.github.com>
2026-02-16 16:50:39 -0600

    End of Session Checkpoint

--- MathGame.Brogment/MathGame.Brogment/Program.cs
+++ MathGame.Brogment/MathGame.Brogment/Program.cs
@@ -1,3 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿
+
+string? readResult;
+Random random = new Random();
+
+int firstOperand = random.Next();
+int secondOperand = random.Next();
+
+do
+{
+    Console.WriteLine("Enter the number key next to the operation you wish to solve: ");
+    Console.WriteLine(@"(1) Addition
+(2) Subtraction
+(3) Multiplication
+(4) Division
+
+(Q) Quit Program");
+
+
+    readResult = Console.ReadLine();
+    readResult = readResult?.ToLower();
+
+    switch (readResult)
+    {
+        case "1":
+            break;
+        case "2":
+            break;
+        case "3":
+            break;
+        case "4":
+            break;
+
+    }
+
+
+}
+while (readResult != "q");
+
+
+static string PerformOperation()
+{
+    return "";
+}
