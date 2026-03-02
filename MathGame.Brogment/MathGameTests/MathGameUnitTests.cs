a9534500daf8049dbbd6b5cf7dfdb8b7d54e459a
brogment <140029632+brogment@users.noreply.github.com>
2026-03-02 15:54:20 -0600

    chore: initilize testing project add on

---
+++ MathGame.Brogment/MathGameTests/MathGameUnitTests.cs
@@ -0,0 +1,13 @@
+﻿using MathGame.Brogment;
+
+namespace MathGameTests
+{
+    [TestClass]
+    public sealed class MathGameUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
